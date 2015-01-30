15fd3ef74677e28f3f44e67440ae8817ef45e481
Emilio Palumbo <emiliopalumbo@gmail.com>
2015-01-30 17:17:43 +0100

    Add script to chech if git branches are up to date

---
+++ scripts/check-git-branch.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+NORMAL="\033[0m"
+
+for b in $@; do
+    LOCAL=$(git rev-parse $b@{0})
+    REMOTE=$(git rev-parse $b@{u})
+    BASE=$(git merge-base $b@{0} $b@{u})
+    
+    echo -en "Branch ${BLUE}$b${NORMAL}: "
+    if [ $LOCAL = $REMOTE ]; then
+        echo -e "${GREEN}Up-to-date${NORMAL}"
+    elif [ $LOCAL = $BASE ]; then
+        echo -e "${YELLOW}Need to pull${NORMAL}"
+    elif [ $REMOTE = $BASE ]; then
+        echo -e "${YELLOW}Need to push${NORMAL}"
+    else
+        echo -e "${RED}Diverged${NORMAL}"
+    fi
+done
