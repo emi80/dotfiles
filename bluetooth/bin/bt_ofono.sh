b2c6a68f52bb191027f303d06aff54021777ee95
Emilio Palumbo <emiliopalumbo@gmail.com>
2020-10-19 14:12:43 +0200

    Add ofono command in bluetooth package

---
+++ bluetooth/bin/bt_ofono.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ofono-phonesim -p 12345 /usr/share/phonesim/default.xml &
+sleep 1
+
+dbus-send --print-reply --system --dest=org.ofono /phonesim org.ofono.Modem.SetProperty string:"Powered" variant:boolean:true
+
+dbus-send --print-reply --system --dest=org.ofono /phonesim org.ofono.Modem.SetProperty string:"Online" variant:boolean:true
